b4a2104f90b2d253422cf3acce496037abf42a68
Cymen Vig <cymenvig@gmail.com>
2012-02-09 13:58:08 -0600

    Working on minimax, still not correct, cleaned up minimax tests

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -5,42 +5,60 @@ module Minimax
   def Minimax.run board
     @initial_turn = Game.turn board
 
-    result = Minimax.minimax board
+    result = {}
+
+    board.empty.each do |space|
+      @special = space == 1
+      board.set space, Game.turn(board)
+      result[space] = Minimax.minimax board
+      if @special
+        puts "board: " + board.to_s + ", initial pick: " + space.to_s
+        puts "result: " + result.to_s
+      end
+      board.clear space
+    end
+
     return result
   end
 
   def Minimax.minimax board, depth = 1
-    
+
     if Game.over? board
-      return Minimax.sign_toggle(board) * Minimax.analysis(board)
+      return Minimax.sign_toggle(board) * Minimax.analysis(board, depth)
     end
-    
+
     player = Game.turn board
     max = -1 * Infinity
 
-    choices = Hash.new
+    z = {}
     board.empty.each do |space|
       board.set space, player
-
-      choices[space] = -Minimax.minimax(board, depth + 1)
-
+      value = -Minimax.minimax(board, depth + 1)
+      max = [value, max].max
+      z[space] = value 
       board.clear space
     end
-    
-    return (depth == 1) ? choices : choices.values.max
+
+    if @special and depth == 1
+      puts "z: " + z.to_s
+    end
+
+    return max
   end
 
-  def Minimax.analysis board
-    if Game.tie? board
+  def Minimax.analysis board, depth
+   if Game.winner? board
+      # the returned value seems backward --
+      return (2 ** -depth) * ((@initial_turn == Game.winner(board)) ? 1 : -1)
+      #return (@initial_turn == Game.winner(board)) ? board.empty.count : -board.empty.count
+      #return ((@initial_turn == Game.winner(board)) ? -1 : 1) * 2 ** -depth
+    elsif Game.tie? board
       return 0
-    elsif Game.winner? board
-      # the returned value seems backward -- 
-      #return (@initial_turn == Game.winner(board)) ? -1 : 1
-      return (@initial_turn == Game.winner(board)) ? board.empty.count : -board.empty.count
     end
+   
   end
 
   def Minimax.sign_toggle board
-    (@initial_turn == Game.winner(board)) ? -1 : 1
+    (@initial_turn == Game.winner(board)) ? 1 : -1
   end
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -1,27 +1,34 @@
 require 'minimax'
+require 'rspec/expectations'
+
+RSpec::Matchers.define :have_optimal_space_of do |expected|
+  match do |actual|
+    (actual.max_by { |key, value| value }).first == expected
+  end
+  failure_message_for_should do |actual|
+    "expected that #{actual.to_s} max value should key equal to #{expected}"
+  end
+end
+
 
 describe Minimax do
-  it ":o should pick space 9 in order to win the game" do
-    result = Minimax.run Board.new [ :x, :x, :o, :x, :x, :o, :o, nil, nil ]
-    #puts "result: " + result.to_s
-    (result.max_by { |key, value| value }).first.should eq(9)
+  it "picks a win over a block" do
+    (Minimax.run Board.new [:x,:x,:o,:x,:x,:o,:o,nil,nil]).should have_optimal_space_of 9
   end
 
-  it ":x should pick space 2 in order to win the game" do
-    result = Minimax.run Board.new [ :x, nil, :x, nil, nil, nil, :o, nil, :o ]
-    #puts "result: " + result.to_s
-    (result.max_by { |key, value| value }).first.should eq(2)
+  it "picks immediate win" do
+    (Minimax.run Board.new [:x,nil,:x,nil,nil,nil,:o,nil,:o]).should have_optimal_space_of 2
   end
 
-  it ":o should pick space 2 in order to block :x from winning" do
-    result = Minimax.run Board.new [ :x, nil, :x, nil, nil, nil, :o, nil, nil ]
-    #puts "result: " + result.to_s
-    (result.max_by { |key, value| value }).first.should eq(2)
+  it "picks a block of opposing player" do
+    (Minimax.run Board.new [:x,nil,:x,nil,nil,nil,:o,nil,nil]).should have_optimal_space_of 2 
   end
 
-  it "something should happen but not sure what" do
-    result = Minimax.run Board.new
-    puts "result: " + result.to_s
-  end
 
+#  # long running test: all spaces are 0
+#  it "something should happen but not sure what" do
+#    result = Minimax.run Board.new
+#    puts "result: " + result.to_s
+#  end
+#
 end
