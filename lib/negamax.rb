b98a7bbf94de8d8634bdfde99a6d8989ff862212
Cymen Vig <cymenvig@gmail.com>
2012-02-10 10:51:09 -0600

    Minor cleanup

--- lib/negamax.rb
+++ lib/negamax.rb
@@ -9,7 +9,7 @@ module Negamax
 
     board.empty.each do |space|
       board.set space, Game.turn(board)
-      result[space] = -Negamax.negamax(board)
+      result[space] = -negamax(board)
       board.clear space
     end
 
@@ -19,7 +19,7 @@ module Negamax
   def Negamax.negamax board, depth = 1, alpha = -Infinity, beta = Infinity
 
     if Game.over? board
-      return Negamax.sign_toggle(board) * Negamax.analysis(board, depth)
+      return sign_toggle(board) * analysis(board, depth)
     end
 
     player = Game.turn board
@@ -27,7 +27,7 @@ module Negamax
 
     board.empty.each do |space|
       board.set space, player
-      max = [-Negamax.negamax(board, depth + 1, -beta, -alpha), max].max
+      max = [-negamax(board, depth + 1, -beta, -alpha), max].max
       board.clear space
       alpha = [alpha, max].max
       return alpha if alpha >= beta
@@ -38,6 +38,7 @@ module Negamax
 
   def Negamax.analysis board, depth
    if Game.winner? board
+      #return 2 ** -depth * (@initial_player == Game.winner(board) ? 1 : 1/2)
       return 2 ** -depth
     elsif Game.tie? board
       return 0
