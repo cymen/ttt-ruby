b00905489994259441f0603413d7c55531f496a6
Cymen Vig <cymenvig@gmail.com>
2012-02-15 08:46:52 -0600

    Work on testing ConsolePrinter (both spec and unit)

--- test/console_printer_test.rb
+++ test/console_printer_test.rb
@@ -1,11 +1,19 @@
 require 'console_printer'
+require 'board'
 require 'test/unit'
 
 # tests
 class ConsolePrinterTest < Test::Unit::TestCase
   def setup
-    @reader = MockReader.new
     @writer = MockWriter.new
+  end
+
+  def test_print_board
+    ConsolePrinter.print_board Board.new([:x,:o,nil,:o]), @writer
+    assert_equal 0, @writer.times_display_blank_line
+    assert_equal 0, @writer.times_display_banner_text
+    assert_equal 0, @writer.times_display_centered_text
+    assert_equal 1, @writer.times_display_board
   end
 end
 
