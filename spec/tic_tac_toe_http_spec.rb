513c222de1b3bc6cc49bca74c99610eac817288d
Cymen Vig <cymenvig@gmail.com>
2012-02-20 11:51:40 -0600

    More testing of HTTP-based tic-tac-toe

--- spec/tic_tac_toe_http_spec.rb
+++ spec/tic_tac_toe_http_spec.rb
@@ -24,4 +24,39 @@ describe 'TicTacToeHttp' do
     follow_redirect!
     last_request.url.should == 'http://example.org/'
   end
+
+  it "redirects back to root after a play is made" do
+    post '/choose_player', params={:player => 'x'}
+    post '/play', params={:space => '1'}
+    last_response.should be_redirect
+    follow_redirect!
+    last_request.url.should == 'http://example.org/'
+  end
+
+  it "stays at root if refreshed" do
+    post '/choose_player', params={:player => 'x'}
+    get '/'
+    last_response.should be_ok
+  end
+
+  it "redirects to choosing player (x or o) if reset" do
+    post '/choose_player', params={:player => 'x'}
+    get '/'
+    last_response.should be_ok
+    get '/reset'
+    last_response.should be_redirect
+    follow_redirect!
+    last_request.url.should == 'http://example.org/'
+    get '/'
+    last_response.should be_redirect
+    follow_redirect!
+    last_request.url.should == 'http://example.org/choose_player'
+  end
+
+  it "redirects to root if game not over" do
+    get '/over'
+    last_response.should be_redirect
+    follow_redirect!
+    last_request.url.should == 'http://example.org/'
+  end
 end
